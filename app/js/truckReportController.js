14f66f40c47d09228fdfaa8a89f0ce8b933f5d1a
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-03 00:33:24 +0600

    summary is prepared

--- app/js/truckReportController.js
+++ app/js/truckReportController.js
@@ -1,25 +1,48 @@
 app.controller('truckReportController', ['$scope', 'truckRepository', truckReportController]);
 
-function truckReportController($scope, repository){
-    $scope.trucks = [];
-
+function truckReportController($scope, repository) {
+    $scope.Trucks = [];
+    $scope.Summary = {};
     repository.getCurrentStatus().then(function (response) {
-        $scope.trucks = response.data;
-
-        
+        $scope.Trucks = response.data;
+        updateSummary();
     });
 
+    function updateSummary() {
+        var trucks = $scope.Trucks;
+        var summary = {
+            Trucks: {Total: trucks.length},
+            Items: {}
+        };
+
+        console.log();
+        ['Moving', 'Not In Use', 'Loading', 'Unloading'].forEach(function (status) {
+            var trucksWithStatus = _.filter(trucks, {Status: status});
+            summary.Trucks[status] = trucksWithStatus.length;
+            summary.Items[status] = trucksWithStatus.reduce(function (memo, truck) {
+                return memo + truck.ItemsCarrying;
+            }, 0);
+        });
+
+
+        summary.Items.Total = trucks.reduce(function (memo, truck) {
+            return memo + truck.ItemsCarrying; // return previous total plus current age
+        }, 0);
+
+        $scope.Summary = summary;
+    }
+
     $scope.getStatusClass = function (truck) {
-        if(truck.Status === 'Moving'){
+        if (truck.Status === 'Moving') {
             return 'success';
         }
-        if(truck.Status === 'Not In Use'){
+        if (truck.Status === 'Not In Use') {
             return 'danger';
         }
-        if(truck.Status === 'Loading'){
+        if (truck.Status === 'Loading') {
             return 'info';
         }
-        if(truck.Status === 'Unloading'){
+        if (truck.Status === 'Unloading') {
             return 'warning';
         }
     };
