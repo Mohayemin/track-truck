3595429714ba668166c62cb1f203c0fe44e07be5
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-04 01:10:01 +0600

    project file fixed

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Models/Response.cs
@@ -0,0 +1,19 @@
+﻿namespace Ssi.TrackTruck.Bussiness.Models
+{
+    public class Response
+    {
+        public bool IsError { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+
+        public static Response Error(string status, string message = null)
+        {
+            return new Response { IsError = true, Status = status, Message = message };
+        }
+
+        public static Response Success(string status = null, string message = null)
+        {
+            return new Response { IsError = false, Status = status, Message = message };
+        }
+    }
+}
