ed18a6879bdef81540f8cb62ad1a56a1dca695d5
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-04 01:49:34 +0600

    Sign in controller added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/signInController.js
@@ -0,0 +1,13 @@
+﻿trackTruck.controller('signInController', [
+    '$scope',
+    signInController
+]);
+
+function signInController($scope) {
+    $scope.model = {};
+
+    $scope.signIn = function() {
+        console.log($scope.model);
+    };
+}
+
