b945cf05eb4acaee51bb1930cc5fd52e172f31ce
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-04 02:16:58 +0600

    Message box added, login is functional except for redirecting.

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/signInController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/signInController.js
@@ -6,10 +6,13 @@
 
 function signInController($scope, authService) {
     $scope.model = {};
-
-    $scope.signIn = function() {
-        authService.signIn($scope.model).then(function(response) {
-            console.log(response);
+    
+    $scope.signIn = function () {
+        $scope.response = null;
+        $scope.message = 'checking...';
+        authService.signIn($scope.model).then(function (response) {
+            $scope.message = null;
+            $scope.response = response;
         });
     };
 }
