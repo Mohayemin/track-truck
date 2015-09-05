6e3eaa2588634c0f07f0f08fac6ee3a8289bab1c
Mohayemin <Mohayemin@localhost>
2015-09-05 21:09:00 +0600

    client side userlist controller added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/userController.js
@@ -0,0 +1,11 @@
+﻿trackTruck.controller('userController', [
+    '$scope',
+    userController
+]);
+
+function userController($scope) {
+    $scope.users = [
+        { Username: 'Admin', Role: 'admin' },
+        { Username: 'Store-1', Role: 'store' }
+    ];
+}
