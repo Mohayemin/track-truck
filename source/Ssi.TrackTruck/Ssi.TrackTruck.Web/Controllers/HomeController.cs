113ffb565afd1060f1f8009e1abc573166a06e9f
Mohayemin <Mohayemin@localhost>
2015-09-05 19:26:08 +0600

    User must be logged in to see home page

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Controllers/HomeController.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web.Mvc;
+using Ssi.TrackTruck.Web.Utils;
 
 namespace Ssi.TrackTruck.Web.Controllers
 {
     public class HomeController : Controller
     {
+        [TrackTruckAuthorize]
         public ActionResult Index()
         {
             return View();
