3cb02c9d5d2df61af0c4bef896a845a375776fa3
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-05 18:02:26 +0600

    Merge with f-auth

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/authService.js
@@ -0,0 +1,17 @@
+﻿trackTruck.factory('authService', [
+    '$http',
+    'url',
+    authService
+]);
+
+function authService($http, url) {
+    function signIn(request) {
+        return $http.post(url('Auth', 'SignIn'), request).then(function(response) {
+            return response.data;
+        });
+    }
+
+    return {
+        signIn: signIn
+    };
+}
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/url.js
@@ -0,0 +1,21 @@
+﻿trackTruck.factory('url', [
+    urlFactory
+]);
+
+function urlFactory() {
+    return function (controller, action, params) {
+        if (!controller) {
+            return '/';
+        }
+        
+        var urlString = '/' + controller + '/' + action;
+        if (params) {
+            urlString = urlString + '?';
+            for (var key in params) {
+                urlString = urlString + key + '=' + params[key] + '&';
+            }
+        }
+
+        return urlString;
+    }
+}
