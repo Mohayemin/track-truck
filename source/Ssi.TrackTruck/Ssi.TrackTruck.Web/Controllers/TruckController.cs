79f33deba4d644610a11cb11cc423f68cbe75835
Mohayemin <Mohayemin@localhost>
2015-09-05 21:31:20 +0600

    Truck report empty view added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Controllers/TruckController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Ssi.TrackTruck.Web.Controllers
+{
+    public class TruckController : Controller
+    {
+        public ActionResult Report()
+        {
+            return View();
+        }
+	}
+}
