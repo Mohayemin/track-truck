16d3cfc54d0da5e127e05322815e3aa836c1d4b3
Mohayemin <Mohayemin@localhost>
2015-09-07 07:21:57 +0600

    Truck status report in progress

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/DeliveryStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Ssi.TrackTruck.Bussiness.DAL
+{
+    public class DeliveryStatus
+    {
+        public static string Moving = "Moving";
+        public static string Loading = "Loading";
+        public static string Unloading = "Unloading";
+        public static string Delivered = "Delivered";
+    }
+}
