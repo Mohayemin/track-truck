d7c2add6f20f9d7934f0e9f84bef7cc104ded91c
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-07 07:21:57 +0600

    Truck status report in progress

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Delivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Entities
+{
+    public class Delivery
+    {
+        public string Id { get; set; }
+        public string DriverName { get; set; }
+        public string TruckNumber { get; set; }
+        public DateTime DeliveryDueTime { get; set; }
+        public int ItemsToDeliver { get; set; }
+        public int? ItemsDeliveredWithoutDamage { get; set; }
+        public string Status { get; set; }
+
+        public string FromOutlet { get; set; }
+        public string ToOutlet { get; set; }
+    }
+}
