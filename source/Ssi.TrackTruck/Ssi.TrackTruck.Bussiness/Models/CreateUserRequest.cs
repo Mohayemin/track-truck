be7afb183e0f16244533f68516991dbd57c1c520
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-07 07:23:17 +0600

    branch for releaseing to app hurbor

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Models/CreateUserRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Ssi.TrackTruck.Bussiness.Models
+{
+    public class CreateUserRequest
+    {
+        public string Username { get; set; }
+        public string InitialPassword { get; set; }
+        public string Role { get; set; }
+
+        public bool Validate()
+        {
+            var nonNullables = new[] {Username, InitialPassword, Role};
+
+            var hasNull = nonNullables.Any(string.IsNullOrEmpty);
+
+            return !hasNull;
+        }
+    }
+}
