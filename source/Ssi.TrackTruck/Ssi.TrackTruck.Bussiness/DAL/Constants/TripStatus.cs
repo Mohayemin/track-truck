afd790b210301e84083f80c8231d6fe67b963378
Mohayemin <Mohayemin@localhost>
2015-09-09 06:42:01 +0600

    Db Models added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Constants/TripStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Ssi.TrackTruck.Bussiness.DAL.Constants
+{
+    public enum TripStatus
+    {
+        OnTheWay = 10,
+        Loading = 20,
+        Unloading = 30
+    }
+}
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Trip.cs
@@ -0,0 +1,17 @@
+﻿using Ssi.TrackTruck.Bussiness.DAL.Constants;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Entities
+{
+    public class Trip
+    {
+        public string Id { get; set; }
+        public int NumberOfItems { get; set; }
+        public string TruckId { get; set; }
+        public TripStatus Status { get; set; }
+        public string FromStoreId { get; set; }
+        public string ToStoreId { get; set; }
+        public string DriverId { get; set; }
+        public string CargoLoaderId { get; set; }
+        public int BillInCents { get; set; }
+    }
+}
