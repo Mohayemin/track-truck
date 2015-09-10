98ae280524cfc56125bce35709e4bcadf778c086
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-11 01:30:48 +0600

    css updated, default values added.

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/tripController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/tripController.js
@@ -7,9 +7,13 @@
 ]);
 
 function tripController($scope, tripService, $filter, dateFormat) {
-    $scope.addRequest = {};
+    $scope.addRequest = {
+        DeliveryHour: 15,
+        DeliveryMinute: 30
+    };
     $scope.addTrip = function () {
         $scope.addRequest.DeliveryDate = $filter('date')($scope.addRequest.DeliveryDate, dateFormat);
+
         tripService.addTrip($scope.addRequest).then(function () {
 
         }).catch(function () {
