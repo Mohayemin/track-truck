2f231f719c020446848b209be2009f18631ddf95
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-11 01:31:50 +0600

    Merge with dev-1.0

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/IRepository.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/IRepository.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using Ssi.TrackTruck.Bussiness.DAL.Entities;
 
 namespace Ssi.TrackTruck.Bussiness.DAL
 {
     public interface IRepository
     {
         T FindOne<T>(Expression<Func<T, bool>> condition);
-        T Create<T>(T user);
+        T Create<T>(T item);
         IQueryable<T> GetAll<T>();
         IQueryable<T> WhereIn<T, TProp>(Expression<Func<T, TProp>> property, IEnumerable<TProp> values);
     }
--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/trackTruckController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/trackTruckController.js
@@ -4,6 +4,5 @@
 ]);
 
 function trackTruckController($scope) {
-
 };
 
