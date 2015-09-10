e034366f50200369b919c4d9e2e259a5ba3da080
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-11 01:16:00 +0600

    Timezone problem fixed

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Trips/AddTripRequest.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Trips/AddTripRequest.cs
@@ -6,17 +6,23 @@ namespace Ssi.TrackTruck.Bussiness.Trips
 {
     public class AddTripRequest
     {
-        public DateTime DeliveryDueTime { get; set; }
         public int NumberOfItems { get; set; }
         public string ClientId { get; set; }
         public string FromWareHouseId { get; set; }
         public string ToBranchId { get; set; }
 
+        public DateTime DeliveryDate { get; set; }
+        public int DeliveryHour { get; set; }
+        public int DeliveryMinute { get; set; }
+
         public Trip ToTrip()
         {
+            var deliveryDueTime = new DateTime(DeliveryDate.Year, DeliveryDate.Month, DeliveryDate.Day,
+                DeliveryHour, DeliveryMinute, 0, DateTimeKind.Unspecified);
+
             return new Trip
             {
-                DeliveryDueTime = DateTime.SpecifyKind(DeliveryDueTime, DateTimeKind.Unspecified),
+                DeliveryDueTime = deliveryDueTime,
                 NumberOfItems = NumberOfItems,
                 Status = TripStatus.NotStarted,
                 ClientId = ClientId,
