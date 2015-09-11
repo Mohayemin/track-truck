a3f88a652ad4639c25c55a494fa980fceff813f8
Mohayemin <Mohayemin@localhost>
2015-09-11 12:02:39 +0600

    Renamed

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Client.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Client.cs
@@ -4,9 +4,9 @@ namespace Ssi.TrackTruck.Bussiness.DAL.Entities
 {
     public class Client
     {
-        public string ClientId { get; set; }
+        public string Id { get; set; }
         public string ClientName { get; set; }
         public int TrucksPerDay { get; set; }
-        public IList<Store> Stores { get; set; }
+        public IList<Branch> Branch { get; set; }
     }
 }
--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Wirehouse.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Wirehouse.cs
@@ -2,7 +2,7 @@
 {
     public class Wirehouse
     {
-        public string WirehouseId { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
     }
