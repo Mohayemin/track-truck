98cb80872c576d54d961ca8c168b3ebc2be3b209
Mohayemin <Mohayemin@localhost>
2015-09-11 23:53:08 +0600

    dummy client is returned from server

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Client.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Client.cs
@@ -5,8 +5,8 @@ namespace Ssi.TrackTruck.Bussiness.DAL.Entities
     public class Client
     {
         public string Id { get; set; }
-        public string ClientName { get; set; }
+        public string Name { get; set; }
         public int TrucksPerDay { get; set; }
-        public IList<Branch> Branch { get; set; }
+        public IList<Branch> Branches { get; set; }
     }
 }
