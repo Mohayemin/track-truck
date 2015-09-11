5aa555ac349654bf464c1fd6854cdd23e1f66939
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-11 23:53:08 +0600

    dummy client is returned from server

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/App_Start/DummyRepository.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/App_Start/DummyRepository.cs
@@ -15,8 +15,24 @@ namespace Ssi.TrackTruck.Web
             var trips = Builder<Trip>.CreateListOfSize(100).Build();
             var trucks = Builder<Truck>.CreateListOfSize(10).Random(random.Next(5, 9)).Do(truck => truck.CurrentTripId = trips[random.Next(99)].Id).Build();
 
+            var clients = new List<Client>
+            {
+                new Client
+                {
+                    Id = "C1",
+                    Name = "Avon",
+                    TrucksPerDay = 15,
+                    Branches = new List<Branch>
+                    {
+                        new Branch{Id = "B1", Name = "Condon", Address = "Condon, Philippines"},
+                        new Branch{Id = "B2", Name = "Quezon", Address = "Quezon, Philippines"}
+                    }
+                }
+            };
+
             data[typeof(Trip)] = (IList)trips;
             data[typeof(Truck)] = (IList)trucks;
+            data[typeof(Client)] = clients;
         }
     }
 }
