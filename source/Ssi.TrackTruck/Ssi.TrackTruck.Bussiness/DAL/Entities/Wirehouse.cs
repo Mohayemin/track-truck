c73f8ffaaaadfc84b7bcc849f053374e5aa62b93
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-11 11:25:21 +0600

    wairhouse entity added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Wirehouse.cs
@@ -0,0 +1,9 @@
+﻿namespace Ssi.TrackTruck.Bussiness.DAL.Entities
+{
+    public class Wirehouse
+    {
+        public string WirehouseId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
