af50813156c8d1323e592bcf7dcae4c62de697bb
Mohayemin <Mohayemin@localhost>
2015-09-12 15:42:08 +0600

    Top-level collections' ID proeprty set

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Truck.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Truck.cs
@@ -1,7 +1,12 @@
-﻿namespace Ssi.TrackTruck.Bussiness.DAL.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Entities
 {
     public class Truck
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Number { get; set; }
         public string CurrentTripId { get; set; }
--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Wirehouse.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/Wirehouse.cs
@@ -1,7 +1,12 @@
-﻿namespace Ssi.TrackTruck.Bussiness.DAL.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Entities
 {
     public class Wirehouse
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
