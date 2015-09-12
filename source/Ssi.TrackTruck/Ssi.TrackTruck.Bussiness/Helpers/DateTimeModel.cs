05052f89e200b9d591604d21870ea0456d20432e
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-12 07:43:50 +0600

    DateTimeModel added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Helpers/DateTimeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ssi.TrackTruck.Bussiness.Helpers
+{
+    public class DateTimeModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Date { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+    }
+}
