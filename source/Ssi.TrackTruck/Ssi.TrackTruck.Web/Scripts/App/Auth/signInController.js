d7d0a6a2153224620274b8754a82a2d052e2c9cd
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-12 12:43:37 +0600

    Merge with dev-1.0

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/signInController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/signInController.js
@@ -4,21 +4,25 @@
     '$window',
     'authService',
     'url',
+    'globalMessage',
     signInController
 ]);
 
-function signInController($scope, $location, $window, authService, url) {
+function signInController($scope, $location, $window, authService, url, globalMessage) {
     $scope.model = {};
 
     $scope.signIn = function () {
         $scope.response = null;
-        $scope.message = 'checking...';
+        globalMessage.info('checking...');
         authService.signIn($scope.model).then(function (response) {
             $scope.message = null;
             $scope.response = response;
 
             if (!response.IsError) {
+                globalMessage.success(response.Message);
                 $window.location.href = url.resolve();
+            } else {
+                globalMessage.error(response.Message);
             }
         });
     };
