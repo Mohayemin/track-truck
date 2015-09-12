028995ad4446769a11ccc54d6949f2ef4688e7cd
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-13 01:09:02 +0600

    clients navigation added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Clients/Client.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Ssi.TrackTruck.Bussiness.DAL.Entities;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Clients
+{
+    public class Client : IEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int TrucksPerDay { get; set; }
+        public IEnumerable<Branch> Branches { get; set; }
+    }
+}
