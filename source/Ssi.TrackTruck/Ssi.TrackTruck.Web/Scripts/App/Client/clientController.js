800849c54baab37a70b32d545f7efa8b95c3b2e0
Mohayemin <Mohayemin@localhost>
2015-09-13 00:20:14 +0600

    list of clients properly displayed

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientController.js
@@ -0,0 +1,13 @@
+﻿trackTruck.controller('clientController', [
+    '$scope',
+    'clientService',
+    clientController
+]);
+
+function clientController($scope, clientService) {
+    clientService.getAllSummary().then(function(clients) {
+        $scope.clients = clients;
+    }).catch(function() {
+        console.error('could not load clients');
+    });
+}
