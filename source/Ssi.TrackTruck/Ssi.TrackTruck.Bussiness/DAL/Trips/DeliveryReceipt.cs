55e25c98711a9dc02fb63421b7535d13ad50f2cd
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-12 15:50:26 +0600

    All reference keys are decoreted with BsonRepresentation attribtues

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Trips/DeliveryReceipt.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Trips/DeliveryReceipt.cs
@@ -1,8 +1,18 @@
-﻿namespace Ssi.TrackTruck.Bussiness.DAL.Trips
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Trips
 {
     public class DeliveryReceipt
     {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
         public string DrNumber { get; set; }
         public int NumberOfBoxes { get; set; }
+
+        public DeliveryReceipt()
+        {
+            Id = ObjectId.GenerateNewId().ToString();
+        }
     }
 }
--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Trips/Trip.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Trips/Trip.cs
@@ -12,12 +12,20 @@ namespace Ssi.TrackTruck.Bussiness.DAL.Trips
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public TripStatus Status { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
         public string ClientId { get; set; }
         public DateTime ExpectedPickupTime { get; set; }
+        
+        [BsonRepresentation(BsonType.ObjectId)]
         public string DriverId { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
         public string WirehouseId { get; set; }
         public long DriverAllowanceInCentavos { get; set; }
         public long DriverSalaryInCentavos { get; set; }
+        
+        [BsonRepresentation(BsonType.ObjectId)]
         public string HelperId { get; set; }
         public long HelperAllowanceInCentavos { get; set; }
         public long HelperSalaryInCentavos { get; set; }
