2c7bbdf8210c48362f73b3d882d0a8bcf7c53fb7
Mohayemin <Mohayemin@localhost>
2015-09-12 06:36:06 +0600

    List of employees are recieved from server

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Employees/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Ssi.TrackTruck.Bussiness.DAL;
+using Ssi.TrackTruck.Bussiness.DAL.Entities;
+
+namespace Ssi.TrackTruck.Bussiness.Employees
+{
+    public class EmployeeService
+    {
+        private readonly IRepository _repository;
+
+        public EmployeeService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _repository.GetAll<Employee>();
+        }
+    }
+}
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Ssi.TrackTruck.Bussiness.Employees;
+
+namespace Ssi.TrackTruck.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeService _employeeService;
+
+        public EmployeeController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+            return Json(_employeeService.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+	}
+}
