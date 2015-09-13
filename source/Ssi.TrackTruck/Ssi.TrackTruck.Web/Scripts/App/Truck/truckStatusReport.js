53f21965cf69a2298bf9514e2c035064aa2261f6
Mohayemin <Mohayemin@localhost>
2015-09-13 23:18:06 +0600

    #11 client side updated

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Truck/truckStatusReport.js
@@ -0,0 +1,32 @@
+﻿trackTruck.directive('truckStatusReport', [
+    'url',
+    'truckService',
+    truckStatusReportDirective
+]);
+
+function truckStatusReportDirective(url, truckService, truckStatus) {
+    return {
+        templateUrl: url.resolveTemplate('Truck', 'truckStatusReport'),
+        scope: {},
+        controller: [
+            '$scope',
+            function ($scope) {
+                $scope.trucks = [];
+                $scope.summary = {};
+
+                $scope.loadReport = function () {
+                    truckService.getCurrentStatus().then(function (trucks) {
+                        $scope.trucks = trucks;
+                        $scope.summary = truckService.calculateReportSummary(trucks);
+                    });
+                };
+
+                $scope.getStatusClass = function (truck) {
+                    return truckStatus[truck.Status].cssClass;
+                };
+
+                $scope.loadReport();
+            }
+        ]
+    };
+}
