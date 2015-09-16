b1e660e74ff4415220554e3b709486afbbc2371e
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-16 23:09:26 +0600

    Sign in page moved and navigations removed from sign in page

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Controllers/PageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Ssi.TrackTruck.Web.Controllers
+{
+    public class PageController : Controller
+    {
+        [Route("")]
+        public ActionResult App()
+        {
+            return View();
+        }
+
+        [Route("signin")]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+	}
+}
