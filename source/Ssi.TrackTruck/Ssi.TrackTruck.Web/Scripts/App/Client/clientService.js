a3a592d9e85c73b69cc9ac37ca685008066b03cb
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-18 07:59:21 +0600

    (#20) client list added to route

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientService.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientService.js
@@ -1,18 +1,16 @@
-﻿trackTruck.factory('clientService', [
+﻿clientModule.factory('clientService', [
     'repository',
-    clientService
+    function clientService(repository) {
+        return {
+            getAll: function () {
+                return repository.get('Client', 'All');
+            },
+            getAllSummary: function () {
+                return repository.get('Client', 'AllSummary');
+            },
+            addClient: function (request) {
+                return repository.post('Client', 'Add', request);
+            }
+        };
+    }
 ]);
-
-function clientService(repository) {
-    return {
-        getAll: function() {
-            return repository.get('Client', 'All');
-        },
-        getAllSummary: function() {
-            return repository.get('Client', 'AllSummary');
-        },
-        addClient: function(request) {
-            return repository.post('Client', 'Add', request);
-        }
-    };
-}
