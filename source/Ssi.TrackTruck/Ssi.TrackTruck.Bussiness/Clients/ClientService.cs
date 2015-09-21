d29874edd86ab95d975bd8a25e051848b4937573
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-21 23:31:29 +0600

    Do not create user when creating branch - server side code removed

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Clients/ClientService.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Clients/ClientService.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Ssi.TrackTruck.Bussiness.Auth;
 using Ssi.TrackTruck.Bussiness.DAL;
 using Ssi.TrackTruck.Bussiness.DAL.Clients;
-using Ssi.TrackTruck.Bussiness.DAL.Entities;
 using Ssi.TrackTruck.Bussiness.Models;
 
 namespace Ssi.TrackTruck.Bussiness.Clients
@@ -11,12 +9,10 @@ namespace Ssi.TrackTruck.Bussiness.Clients
     public class ClientService
     {
         private readonly IRepository _repository;
-        private readonly AuthService _authService;
 
-        public ClientService(IRepository repository, AuthService authService)
+        public ClientService(IRepository repository)
         {
             _repository = repository;
-            _authService = authService;
         }
 
         public IEnumerable<Client> GetAll()
@@ -41,12 +37,6 @@ namespace Ssi.TrackTruck.Bussiness.Clients
                 {
                     return Response.Error("", "Two or more branches has the same name");
                 }
-                if (BranchUsernameTaken(request))
-                {
-                    return Response.Error("", "One or more branch usernames are already taken");
-                }
-                var users = request.Branches.Select(b => _authService.CreateUserObject(b.Username, b.Password, Role.BranchCustodian));
-                _repository.CreateAll(users);
             }
 
             var branches = request.Branches.Select(b => b.ToBranch());
@@ -75,14 +65,7 @@ namespace Ssi.TrackTruck.Bussiness.Clients
             var branchNameDuplicate = branchNames.Distinct().Count() != branchNames.Count;
             return branchNameDuplicate;
         }
-
-        private bool BranchUsernameTaken(AddClientRequest request)
-        {
-            var branchUsernames = request.Branches.Select(b => b.Username);
-            var usernameTaken = _repository.WhereIn<User, string>(u => u.Username, branchUsernames).Any();
-            return usernameTaken;
-        }
-
+        
         public Response Delete(string id)
         {
             var client = _repository.SoftDelete<Client>(id);
