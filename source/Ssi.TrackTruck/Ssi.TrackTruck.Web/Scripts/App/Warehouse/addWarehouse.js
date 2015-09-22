04044dc509a6beaad56fb0b14dff847b96a5c2ae
Mohayemin <Mohayemin@localhost>
2015-09-22 22:26:30 +0600

    global message added

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Warehouse/addWarehouse.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Warehouse/addWarehouse.js
@@ -8,14 +8,18 @@
             controller: [
                 '$scope',
                 'warehouseService',
+                'globalMessage',
                 function ($scope,
-                    warehouseService) {
+                    warehouseService,
+                    globalMessage) {
                     $scope.request = {};
-                    $scope.add = function() {
+                    $scope.add = function () {
+                        globalMessage.info('adding warehouse');
                         warehouseService.add($scope.request).then(function () {
                             $location.url('warehouse/list');
+                            globalMessage.success('warehouse added');
                         }).catch(function (message) {
-                            console.error(message);
+                            globalMessage.error(message);
                         });
                     };
                 }
