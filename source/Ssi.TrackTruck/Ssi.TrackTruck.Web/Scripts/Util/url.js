e2cd144dbb584baf6ea377ec70b6c3888857bea6
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-23 00:01:35 +0600

    Merge with dev-1.0

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/url.js
@@ -0,0 +1,29 @@
+﻿utilModule.factory('url', [
+    function url() {
+        return {
+            template: function(module, feature) {
+                var root = '/Scripts/App/';
+                return root + module + '/' + feature + '.html';
+            },
+            route: function() {
+                var paths = Array.prototype.slice.call(arguments);
+                return '#/' + paths.join('/');
+            },
+            server: function(controller, action, params) {
+                if (!controller) {
+                    return '/';
+                }
+
+                var urlString = '/' + controller + '/' + action;
+                if (params) {
+                    urlString = urlString + '?';
+                    for (var key in params) {
+                        urlString = urlString + key + '=' + params[key] + '&';
+                    }
+                }
+
+                return urlString;
+            }
+        };
+    }
+]);
