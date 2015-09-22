fb8a27bbe89944070c967b92fb7b09a8ad2ee1f9
tajkiatoma <tajkiatoma@localhost>
2015-09-22 23:13:06 +0600

    client side prepared for password changing

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/changePassword.js
@@ -0,0 +1,19 @@
+﻿authModule.directive('changePassword', [
+    'url',
+    function(url) {
+        return {
+            templateUrl: url.template('Auth', 'changePassword'),
+            scope: {},
+            controller: [
+                '$scope',
+                function ($scope) {
+                    $scope.request = {};
+
+                    $scope.changePassword = function() {
+
+                    };
+                }
+            ]
+        };
+    }
+]);
