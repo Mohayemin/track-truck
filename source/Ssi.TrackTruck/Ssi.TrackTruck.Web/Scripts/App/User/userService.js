3577606424a61ba1a8119b47fb464c69be165d3e
Mohayemin <Mohayemin@localhost>
2015-09-24 10:07:20 +0600

    add method added in user service

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/User/userService.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/User/userService.js
@@ -1,12 +1,36 @@
 ﻿userModule.factory('userService', [
     'repository',
-    function userService(repository) {
-        function getAll() {
-            return repository.get('User', 'All');
-        }
+    '$q',
+    function userService(repository
+        , $q) {
+        var _loadPromise = null;
+        var _users = [];
 
-        return {
-            getAll: getAll
+        var service = {
+            getAll: function (force) {
+                if (!_loadPromise || force) {
+                    _loadPromise = repository.get('User', 'All').then(function (users) {
+                        _users.length = 0;
+                        _users.push.apply(_users, users);
+                        return _users;
+                    });
+                }
+                return _loadPromise;
+            },
+            add: function(request) {
+                return repository.post('User', 'Add', request).then(function(response) {
+                    if (response.IsError) {
+                        return $q.reject(response.Message);
+                    }
+                    return service.getAll().then(function(users) {
+                        var user = response.Data;
+                        users.push(user);
+                        return user;
+                    });
+                });
+            }
         };
+
+        return service;
     }
 ]);
