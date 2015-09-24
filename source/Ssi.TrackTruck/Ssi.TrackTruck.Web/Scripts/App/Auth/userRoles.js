05a1b7cc8982547f35df5ed84f9694c118b4c7e6
Mohayemin <Mohayemin@localhost>
2015-09-24 08:35:42 +0600

    Roles added in client side, updated in server side

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Auth/userRoles.js
@@ -0,0 +1,8 @@
+﻿/**
+* Must synch with enum Role in serverside
+*/
+authModule.value('userRoles', [
+    { label: 'Admin', value: 1 },
+    { label: 'Branch Custodian', value: 2 },
+    { label: 'Encoder', value: 4 }
+]);
