7f1f22d69e862876649a8b569a637edf2e60eea6
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-24 07:37:24 +0600

    (#28) Firstname and lastname added in DbUser

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/DbUser.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/DbUser.cs
@@ -4,6 +4,7 @@ using Ssi.TrackTruck.Bussiness.Auth;
 
 namespace Ssi.TrackTruck.Bussiness.DAL.Entities
 {
+    [BsonIgnoreExtraElements(true)]
     public class DbUser : IEntity
     {
         [BsonId]
@@ -11,6 +12,8 @@ namespace Ssi.TrackTruck.Bussiness.DAL.Entities
         public string Id { get; set; }
         public string Username { get; set; }
         public string UsernameLowerCase { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string PasswordHash { get; set; }
         [BsonRepresentation(BsonType.String)]
         public Role Role { get; set; }
