e0214a98577b57d1c48be0a4965e262b82a1576f
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-09-24 08:20:06 +0600

    add user directive added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/User/addUser.js
@@ -0,0 +1,35 @@
+﻿userModule.directive('addUser', [
+    'url',
+    function (
+        url
+        ) {
+        var directive = {
+            templateUrl: url.template('User', 'addUser'),
+            scope: {},
+            controller: [
+                '$scope',
+                'userService',
+                'globalMessage',
+                function ($scope
+                    , userService
+                    , globalMessage) {
+
+                    $scope.request = {
+                        
+                    };
+                    $scope.add = function () {
+                        globalMessage.info('adding user', 0);
+                        userService.add($scope.request).then(function () {
+                            $location.url('user/list');
+                            globalMessage.success('user added');
+                        }).catch(function (message) {
+                            globalMessage.error(message);
+                        });
+                    };
+                }
+            ]
+        };
+
+        return directive;
+    }
+]);
