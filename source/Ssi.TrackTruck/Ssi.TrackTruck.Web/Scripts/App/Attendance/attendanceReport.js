0612e99fed2017b5d6b701546d38561923d940ab
Mohayemin <Mohayemin@localhost>
2015-10-02 19:44:34 +0600

    attendance done

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Attendance/attendanceReport.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Attendance/attendanceReport.js
@@ -12,17 +12,43 @@
                 function ($scope,
                     attendanceService
                 ) {
+                    var today = moment();
+                    var yesterday = moment().add(-1, 'd');
+
                     $scope.filter = {
-                        fromDate: {},
-                        toDate: {}
+                        fromDate: {
+                            year: yesterday.year(),
+                            month: yesterday.month(),
+                            day: yesterday.date()
+                        },
+                        toDate: {
+                            year: today.year(),
+                            month: today.month(),
+                            day: today.date()
+                        }
                     };
 
-                    
-
                     $scope.loadReport = function () {
-                        attendanceService.getReport($scope.filter).then(function(data) {
-                            $scope.reportRows = data;
-                        });
+                        var fd = $scope.filter.fromDate;
+                        var td = $scope.filter.toDate;
+                        var start = moment(new Date(fd.year, fd.month - 1, fd.day));
+                        var end = moment(new Date(td.year, td.month - 1, td.day));
+
+                        if (start <= end) {
+                            attendanceService.getReport($scope.filter).then(function (data) {
+                                $scope.reportRows = data;
+                            });
+
+                            var current = start;
+                            $scope.dates = [];
+                            while (current <= end) {
+                                $scope.dates.push({
+                                    value: current.format('YYYYMMDD'),
+                                    label: current.format('D-MMM-YY'),
+                                });
+                                current.add(1, 'd');
+                            }
+                        }
                     };
                 }
             ]
