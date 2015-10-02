8c3066332a04f48bfd34acaf9ff704380de5e552
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-02 15:04:40 +0600

    a separate collection created for storing daily hits

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Entities/DbDailyHit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Entities
+{
+    public class DbDailyHit
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; }
+        public int Date { get; set; }
+        public string UserId { get; set; }
+        public List<int> HitTimes { get; set; }
+    }
+}
