5444b0d5aba7349f72503379ca0876866414aee9
Mohayemin <Mohayemin@localhost>
2015-10-03 13:39:30 +0600

    DbClient updated to accept list of addresses

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Clients/DbClient.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Clients/DbClient.cs
@@ -14,5 +14,6 @@ namespace Ssi.TrackTruck.Bussiness.DAL.Clients
         public int TrucksPerDay { get; set; }
         public IEnumerable<DbBranch> Branches { get; set; }
         public bool IsDeleted { get; set; }
+        public List<string> Address { get; set; }
     }
 }
