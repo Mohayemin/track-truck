eea4956c5cda3cba112480e0b0af529ecc0ad80c
tajkiatoma <tajkiatoma@localhost>
2015-10-03 11:28:54 +0600

    raw data loaded

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Truck/truckList.js
@@ -0,0 +1,20 @@
+﻿truckModule.directive('truckList', [
+    'url',
+    function(url) {
+        return {
+            templateUrl: url.template('Truck', 'truckList'),
+            scope: {},
+            controller: [
+                '$scope',
+                'truckService',
+                function($scope,
+                    truckService) {
+
+                    truckService.getAll().then(function(trucks) {
+                        $scope.trucks = trucks;
+                    });
+                }
+            ]
+        };
+    }
+]);
