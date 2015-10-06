9ac8e01c67db67868cb575627459bad054fa2846
tajkiatoma <tajkiatoma@localhost>
2015-10-06 21:51:51 +0600

    common directive loaded for delete operations

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/deleteButtonDirective.js
@@ -0,0 +1,24 @@
+﻿utilModule.directive('deleteButton', [
+    'url',
+    'repository',
+    function(url,
+        repository) {
+        return {
+            templateUrl: '/Scripts/Util/deleteButton.html',
+            scope: {
+                controller: '=',
+                action: '=',
+                itemId: '='
+            },
+            controller: [
+                '$scope',
+                function ($scope) {
+                    $scope.delete = function () {
+                        console.log($scope.controller, $scope.action, $scope.itemId);
+                        repository.post($scope.controller, $scope.action, $scope.itemId);
+                    }
+                }
+            ]
+        };
+    }
+]);
