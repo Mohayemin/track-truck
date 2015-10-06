21ebb38a9ab890891cc9f4884275f2a367aaf94c
tajkiatoma <tajkiatoma@localhost>
2015-10-06 23:20:22 +0600

    truck deleted

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Trucks/TruckService.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Trucks/TruckService.cs
@@ -20,7 +20,7 @@ namespace Ssi.TrackTruck.Bussiness.Trucks
         public IEnumerable<TruckStatusItem> GetCurrentStatus()
         {
             var allTrucks =
-                _repository.GetAll<DbTruck>();
+                _repository.GetAllUndeleted<DbTruck>();
 
             var tripIds = allTrucks.Select(truck => truck.CurrentTripId);
 
@@ -50,7 +50,13 @@ namespace Ssi.TrackTruck.Bussiness.Trucks
 
         public IEnumerable<DbTruck> GetAll()
         {
-            return _repository.GetAll<DbTruck>();
+            return _repository.GetAllUndeleted<DbTruck>();
+        }
+
+        public Response Delete(string id)
+        {
+            _repository.SoftDelete<DbTruck>(id);
+            return Response.Success(null, "Successfully deleted");
         }
     }
 }
