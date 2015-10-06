17b89c0b4df7e04088e30bed33e93c820b565e81
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-06 18:38:56 +0600

    Merge with dev-1.0

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Clients/AddBranchRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Ssi.TrackTruck.Bussiness.DAL.Clients;
+
+namespace Ssi.TrackTruck.Bussiness.Clients
+{
+    public class AddBranchRequest
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public bool Validate()
+        {
+            var fields = new[] { Name, Address};
+
+            var valid = fields.All(s => !string.IsNullOrWhiteSpace(s));
+
+            return valid;
+        }
+
+        public DbBranch ToBranch()
+        {
+            return new DbBranch { Name = Name, Address = Address };
+        }
+
+    }
+}
