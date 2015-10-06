88d1e2a2d18a47a6c36d41b468da2da0cbf6ce23
tajkiatoma <tajkiatoma@localhost>
2015-10-06 23:03:55 +0600

    employee deleted

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/deleteButtonDirective.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/deleteButtonDirective.js
@@ -1,21 +1,35 @@
 ﻿utilModule.directive('deleteButton', [
     'url',
     'repository',
+    'globalMessage',
     function(url,
-        repository) {
+        repository,
+        globalMessage) {
         return {
             templateUrl: '/Scripts/Util/deleteButton.html',
             scope: {
                 controller: '=',
-                action: '=',
-                itemId: '='
+                list: '=',
+                index: '='
             },
             controller: [
                 '$scope',
                 function ($scope) {
                     $scope.delete = function () {
-                        console.log($scope.controller, $scope.action, $scope.itemId);
-                        repository.post($scope.controller, $scope.action, $scope.itemId);
+                        var action = 'Delete';
+                        var data = {
+                            id : $scope.list[$scope.index].Id
+                        };
+                        repository.post($scope.controller, action, data).then(function (response) {
+                            if (!response.IsError) {
+                                $scope.list.splice($scope.index, 1);
+                                globalMessage.success(response.Message);  
+                            } else {
+                                globalMessage.error(response.Message);
+                            }
+                        }).catch(function(message) {
+                            globalMessage.error(message);
+                        });
                     }
                 }
             ]
