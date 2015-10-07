a6730cce939fdc110ede4078b63f749d34ab0723
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-07 22:56:38 +0600

    datepicker updated

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/datetimeDirective.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/datetimeDirective.js
@@ -13,8 +13,18 @@
                 function ($scope
                     , $filter) {
                     var model = $scope.model;
-                    
-                    $scope.$watch('datePickerDate', function(dpd) {
+
+                    if (model && model.year) {
+                        $scope.datePickerDate = new Date(
+                            model.year || 0
+                            , model.month || 0
+                            , model.day || 0
+                            , model.hour || 0
+                            , model.minute || 0
+                            , 0);
+                    }
+
+                    $scope.$watch('datePickerDate', function (dpd) {
                         if (dpd) {
                             model.year = dpd.getFullYear();
                             model.month = dpd.getMonth() + 1;
