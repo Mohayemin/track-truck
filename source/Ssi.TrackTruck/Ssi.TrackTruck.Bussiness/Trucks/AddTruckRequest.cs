24d063ef69b31ba2c57b737dee573339f0cec303
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-09 15:26:21 +0600

    branch for receiving a trip by branch custodian. (#15)

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Trucks/AddTruckRequest.cs
@@ -0,0 +1,32 @@
+﻿using Ssi.TrackTruck.Bussiness.DAL.Entities;
+using Ssi.TrackTruck.Bussiness.Models;
+
+namespace Ssi.TrackTruck.Bussiness.Trucks
+{
+    public class AddTruckRequest
+    {
+        public string RegistrationNumber { get; set; }
+        public string DriverId { get; set; }
+        public string HelperId { get; set; }
+
+        public DbTruck ToTruck()
+        {
+            return new DbTruck
+            {
+                CurrentTripId = null,
+                RegistrationNumber = RegistrationNumber,
+                DriverId = DriverId,
+                HelperId = HelperId
+            };
+        }
+
+        public Response Validate()
+        {
+            if (string.IsNullOrWhiteSpace(RegistrationNumber))
+            {
+                return Response.ValidationError("Registration number must be specified");
+            }
+            return Response.Success();
+        }
+    }
+}
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Attendance/attendanceService.js
@@ -0,0 +1,24 @@
+﻿attendanceModule.factory('attendanceService', [
+    'repository',
+    'userService',
+    function (
+        repository,
+        userService
+        ) {
+        var service = {
+            getReport: function (filter) {
+                return repository.post('Attendance', 'Report', filter).then(function (rows) {
+                    userService.getIndexedUsers().then(function(userIndex) {
+                        rows.forEach(function (row) {
+                            row.FullName = (userIndex[row.UserId] || {}).FullName;
+                        });
+                    });
+
+                    return rows;
+                });
+            }
+        };
+
+        return service;
+    }
+]);
