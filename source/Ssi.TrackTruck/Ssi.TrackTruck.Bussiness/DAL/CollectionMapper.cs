796f9c33fb3cb393936148558997ccb10eb8524b
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-09 09:46:23 +0600

    branch for trip report

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/CollectionMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Ssi.TrackTruck.Bussiness.DAL.Clients;
+using Ssi.TrackTruck.Bussiness.DAL.Entities;
+using Ssi.TrackTruck.Bussiness.DAL.Trips;
+using Ssi.TrackTruck.Bussiness.DAL.Users;
+
+namespace Ssi.TrackTruck.Bussiness.DAL
+{
+    public class CollectionMapper
+    {
+        private Dictionary<Type, string> _mapping;
+
+        public CollectionMapper()
+        {
+            _mapping = new Dictionary<Type, string>
+            {
+                {typeof(DbClient), "clients"},
+                {typeof(DbEmployee), "employees"},
+                {typeof(DbTruck), "trucks"},
+                {typeof(DbUser), "users"},
+                {typeof(DbWarehouse), "warehouses"},
+                {typeof(DbTrip), "trips"},
+                {typeof(DbDailyHit), "dailyHit"}
+            };
+        }
+
+        public string Map(Type type)
+        {
+            return _mapping[type];
+        }
+    }
+}
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Employee/addEmployee.js
@@ -0,0 +1,41 @@
+﻿employeeModule.directive('addEmployee', [
+    'url',
+    '$location',
+    function (url,
+        $location) {
+            return {
+            templateUrl: url.template('Employee', 'addEmployee'),
+            scope: {},
+            controller: [
+                '$scope',
+                'employeeService',
+                'designation',
+                function ($scope, employeeService, designation) {
+                    $scope.designations = {
+                        values: [designation.supervisor, designation.driver, designation.helper],
+                        selected: ''
+                    };
+
+                    function isValid() {
+                        if (employeeService.isDesignationEmpty($scope.designations.selected)) {
+                            console.log('designation cannot be empty');
+                            return false;
+                        }
+                        $scope.request.Designation = $scope.designations.selected;
+                        return true;
+                    }
+
+                    $scope.add = function () {
+                        if (isValid()) {
+                            employeeService.add($scope.request).then(function () {
+                                $location.url('employee/list');
+                            }).catch(function () {
+                                console.error('could not add employee');
+                            });
+                        }
+                    }
+                }
+            ]
+        };
+    }
+]);
