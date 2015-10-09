6b30babe488c51ea00b2c1566132aac155b92df5
Mohayemin <Mohayemin@localhost>
2015-10-09 18:52:55 +0600

    a controller has been added to root of the app. Global objects will be bound to this controller's scope.

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Truck/truckListController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Truck/truckListController.js
@@ -1,11 +1,9 @@
 ﻿truckModule.controller('truckListController', [
     '$scope',
     'truckService',
-    'url',
     function ($scope
         , truckService
-        , url) {
-        $scope.url = url;
+        ) {
 
         truckService.getAll().then(function (trucks) {
             $scope.trucks = trucks;
