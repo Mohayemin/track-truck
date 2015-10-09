cab23a540c971a522edd7948775868ff33ec67c4
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-09 18:53:34 +0600

    Merge with dev-refactor

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Trips/DbDeliveryReceipt.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ssi.TrackTruck.Bussiness.DAL.Trips
+{
+    [BsonIgnoreExtraElements]
+    public class DbDeliveryReceipt 
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string DrNumber { get; set; }
+        public int NumberOfBoxes { get; set; }
+
+        public DbDeliveryReceipt()
+        {
+            Id = ObjectId.GenerateNewId().ToString();
+        }
+    }
+}
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Models/ChangePasswordRequest.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Ssi.TrackTruck.Bussiness.Models
+{
+    public class ChangePasswordRequest
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+
+        public Response Validate()
+        {
+            if (string.IsNullOrWhiteSpace(CurrentPassword))
+            {
+                return Response.ValidationError("Current password cannot be empty");
+            }
+            if (string.IsNullOrWhiteSpace(NewPassword))
+            {
+                return Response.ValidationError("New password cannot be empty");
+            }
+            if (string.IsNullOrWhiteSpace(ConfirmNewPassword))
+            {
+                return Response.ValidationError("Please retype the new password");
+            }
+            if (NewPassword != ConfirmNewPassword)
+            {
+                return Response.ValidationError("New password and confirmation of new password doesn't match");
+            }
+
+            return Response.Success();
+        }
+    }
+}
