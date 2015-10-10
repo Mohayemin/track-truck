21916089fedc1631669d66d84245a0de1a16ba1c
tajkiatoma <tajkiatoma@localhost>
2015-10-10 15:33:49 +0600

    js and html prepared

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Truck/truckEditController.js
@@ -0,0 +1,35 @@
+﻿truckModule.controller('truckEditController', [
+    '$scope',
+    '$routeParams',
+    'truckService',
+    'employeeService',
+    'designation',
+    '$location',
+    function ($scope,
+        $routeParams,
+        truckService,
+        employeeService,
+        designation,
+        $location) {
+
+        truckService.get($routeParams['id']).then(function (truck) {
+            $scope.request = JSON.parse(JSON.stringify(truck));
+        });
+
+        employeeService.getAllByDesignation(designation.driver).then(function (drivers) {
+            $scope.drivers = drivers;
+            $scope.request.driver = _.find($scope.drivers, { Id: $scope.request.DriverId });
+        });
+
+        employeeService.getAllByDesignation(designation.helper).then(function (helpers) {
+            $scope.helpers = helpers;
+            $scope.request.helper = _.find($scope.helpers, { Id: $scope.request.HelperId });
+        });
+
+        $scope.save = function () {
+            truckService.edit($scope.request).then(function () {
+                $location.url('truck/list');
+            });
+        }
+    }
+]);
