81e072ddef621c1616b16386c035e21859635b83
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-10 13:56:19 +0600

    input number of rejected boxes are shown

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/tripTransactionController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/tripTransactionController.js
@@ -1,13 +1,19 @@
 ﻿tripModule.controller('tripTransactionController', [
     '$scope',
     'tripService',
+    'collection',
     function (
         $scope
         , tripService
+        , collection
         ) {
 
         tripService.getMyActiveDrops().then(function (drops) {
             $scope.drops = drops;
         });
+
+        $scope.totalRejected = function(drop) {
+            return collection.sum(drop.DeliveryReceipts, 'RejectedNumberOfBoxes');
+        };
     }
 ]);
