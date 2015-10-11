b7afb478d8a0a30f3e4febf7b6036bc02123cba3
Mohayemin <Mohayemin@localhost>
2015-10-11 23:36:24 +0600

    list of navigations separated

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/url.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/url.js
@@ -19,30 +19,33 @@
             return url;
         }
 
+        function server(controller, action, params) {
+            if (!controller) {
+                return '/';
+            }
+
+            var urlString = '/' + controller + '/' + action;
+            if (params) {
+                urlString = urlString + '?';
+                for (var key in params) {
+                    urlString = urlString + key + '=' + params[key] + '&';
+                }
+            }
+
+            return urlString;
+        }
+
         this.route = route;
         this.template = template;
         this.path = path;
+        this.server = server;
 
         this.$get = [
             function() {
                 return {
                     route: route,
                     template: template,
-                    server: function (controller, action, params) {
-                        if (!controller) {
-                            return '/';
-                        }
-
-                        var urlString = '/' + controller + '/' + action;
-                        if (params) {
-                            urlString = urlString + '?';
-                            for (var key in params) {
-                                urlString = urlString + key + '=' + params[key] + '&';
-                            }
-                        }
-
-                        return urlString;
-                    }
+                    server: server
                 };
             }
         ];
