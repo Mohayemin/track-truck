dccfd1c821820488d6e3d7d7635c6b75f21be914
tajkiatoma <tajkiatoma@localhost>
2015-10-14 23:40:41 +0600

    js and html prepared

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientEditController.js
@@ -0,0 +1,19 @@
+﻿clientModule.controller('clientEditController', [
+    '$scope',
+    '$routeParams',
+    'clientService',
+    '$location',
+    function ($scope,
+        $routeParams,
+        clientService,
+        $location) {
+
+        clientService.get($routeParams['id']).then(function (client) {
+            $scope.request = JSON.parse(JSON.stringify(client));
+        });
+
+        $scope.save = function() {
+            $location.url('client/list');
+        }
+    }
+]);
