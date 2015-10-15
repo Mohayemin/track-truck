02a5ae33d12c5923816acbb54391f9d2e510fb45
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-16 00:15:06 +0600

    page title directive added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/pageTitleDirective.js
@@ -0,0 +1,10 @@
+﻿utilModule.directive('pageTitle', [
+    function() {
+        var directive = {
+            templateUrl: '/Scripts/Util/pageTitle.html',
+            transclude: true
+        };
+
+        return directive;
+    }
+]);
