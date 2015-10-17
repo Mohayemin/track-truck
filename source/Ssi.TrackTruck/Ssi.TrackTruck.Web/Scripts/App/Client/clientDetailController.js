2337373ceca001d07346e4717f0f7dac41268ac1
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-17 19:20:16 +0600

    Merge with dev-1.0

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientDetailController.js
@@ -0,0 +1,44 @@
+﻿clientModule.controller('clientDetailController', [
+    '$scope',
+    '$routeParams',
+    'clientService',
+    'userService',
+    '$window',
+    '$location',
+    'globalMessage',
+    function ($scope, $routeParams, clientService,
+        userService,
+        $window,
+        $location,
+        globalMessage) {
+        function init(client) {
+            $scope.client = client;
+
+            userService.getIndexedUsers().then(function(userIndex) {
+                $scope.client.Branches.forEach(function(branch) {
+                    branch.CustodianUser = userIndex[branch.CustodianUserId];
+                });
+            });
+
+
+            $scope.PickupAddressDisplay = client.Addresses.map(function (address) {
+                return address.Text;
+            }).join(', ');
+
+            $scope.deleteClient = function() {
+                if ($window.confirm('Are you sure you want to delete this client?')) {
+                    clientService.delete($scope.client).then(function() {
+                        $location.url('client/list');
+                        globalMessage.warning('client deleted');
+                    }).catch(function(message) {
+                        globalMessage.error(message);
+                    });
+                }
+            };
+        }
+
+        clientService.get($routeParams['id']).then(function(client) {
+            init(client);
+        });
+    }
+]);
