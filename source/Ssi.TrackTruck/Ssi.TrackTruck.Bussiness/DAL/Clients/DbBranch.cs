92fd119b2a48b1e1dd0a232001e0139c49e60fbb
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-17 18:31:55 +0600

    Merge with f-edit-client

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Clients/DbBranch.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/Clients/DbBranch.cs
@@ -3,6 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Ssi.TrackTruck.Bussiness.DAL.Clients
 {
+    [BsonIgnoreExtraElements]
     public class DbBranch
     {
         [BsonRepresentation(BsonType.ObjectId)]
