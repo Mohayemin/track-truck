a16c05bb7a7e6d9c1dd675264a6d4eeef31efe90
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-17 21:30:21 +0600

    Merge with dev-refactor

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientListController.js
@@ -0,0 +1,14 @@
+﻿clientModule.controller('clientListController', [
+    '$scope',
+    'clientService',
+    function($scope,
+        clientService) {
+        clientService.getAll().then(function(clients) {
+            $scope.clients = clients;
+        });
+
+        $scope.deleteItem = function(clientId) {
+            return clientService.delete(clientId);
+        };
+    }
+]);
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Employee/employeeService.js
@@ -0,0 +1,87 @@
+﻿employeeModule.factory('employeeService', [
+    'repository',
+    'buildIdMap',
+    '$q',
+    '_',
+    function employeeService(
+        repository,
+        buildIdMap,
+        $q,
+        _) {
+        var _employees = [];
+        var _employeeById = {};
+        var _loadPromise = null;
+
+        function getAll(force) {
+            if (!_loadPromise || force) {
+                _loadPromise = repository.get('Employee', 'All').then(function (employees) {
+                    _employees.length = 0;
+                    _employees.push.apply(_employees, employees);
+                    _employeeById = buildIdMap(_employees);
+
+                    return _employees;
+                });
+            }
+            return _loadPromise;
+        }
+
+        var service = {
+            getAllByDesignation: function(designation) {
+                return service.getAll().then(function() {
+                    return _.where(_employees, { Designation: designation });
+                });
+            },
+            getAll: getAll,
+            get: function (id) {
+                return getAll().then(function () {
+                    return _.find(_employees, { Id: id });
+                });
+            },
+            'delete': function (id) {
+                return repository.post('Employee', 'Delete', { id: id }).then(function (response) {
+                    if (!response.IsError) {
+                        var index = _.findIndex(_employees, { Id: id });
+                        if (index >= 0) {
+                            _employees.splice(index, 1);
+                            delete _employeeById[id];
+                        }
+                        return response;
+                    }
+
+                    return $q.reject(response.Message || response.status || 'could not delete client');
+                });
+            },
+            add: function (request) {
+                return repository.post('Employee', 'Add', request).then(function (response) {
+                    if (response.IsError) {
+                        return $q.reject(response.Message || response.Status || 'Could not add employee');
+                    }
+                    var employee = response.Data;
+                    _employees.push(employee);
+                    return employee;
+                });
+            },
+            edit: function(request) {
+                return repository.post('Employee', 'Save', request).then(function(response) {
+                    if (response.IsError) {
+                        return $q.reject(response.Message || response.Status || 'Could not edit employee');
+                    }
+
+                    var employee = _employeeById[request.Id];
+                    angular.extend(employee, response.Data);
+                    return employee;
+                });
+            },
+            isDesignationEmpty: function(designation) {
+                return designation === undefined || designation === '';
+            },
+            getIndexedEmployees: function () {
+                return service.getAll().then(function () {
+                    return _employeeById;
+                });
+            }
+        }
+
+        return service;
+    }
+]);
