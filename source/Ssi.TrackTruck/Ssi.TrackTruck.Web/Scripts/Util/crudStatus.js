15a20cf32ba6d8a3450b3f4d536ffa8a27b417ce
Mohayemin <Mohayemin@localhost>
2015-10-17 10:31:04 +0600

    crudStatus classes and services added to keep track of modification status

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/crudStatus.js
@@ -0,0 +1,44 @@
+﻿// Keep sync with CrudStatus.cs
+utilModule.factory('CrudStatus', [
+    function () {
+        var service = {
+            unchanged: 'Unchanged',
+            added: 'Added',
+            edited: 'Edited',
+            deleted: 'Deleted',
+
+            getStatusOnEdit: function (originalStatus) {
+                if (originalStatus == service.unchanged) {
+                    return service.edited;
+                }
+                if (originalStatus == service.added) {
+                    return service.added;
+                }
+                if (originalStatus == service.edited) {
+                    return service.edited;
+                }
+                if (originalStatus == service.deleted) {
+                    return service.deleted;
+                }
+                throw 'invalid original status: ' + originalStatus;
+            },
+            getStatusOnDelete: function(originalStatus) {
+                if (originalStatus == service.unchanged) {
+                    return service.deleted;
+                }
+                if (originalStatus == service.added) {
+                    return service.unchanged;
+                }
+                if (originalStatus == service.edited) {
+                    return service.deleted;
+                }
+                if (originalStatus == service.deleted) {
+                    return service.deleted;
+                }
+                throw 'invalid original status: ' + originalStatus;
+            }
+        };
+
+        return service;
+    }
+]);
