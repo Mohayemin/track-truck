13530d45374c7ab21ca647eb0b653eea2903c542
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-17 15:47:21 +0600

    Serverside edit models added

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Clients/EditBranchRequest.cs
@@ -0,0 +1,10 @@
+﻿using Ssi.TrackTruck.Bussiness.Helpers;
+
+namespace Ssi.TrackTruck.Bussiness.Clients
+{
+    public class EditBranchRequest : AddBranchRequest
+    {
+        public string Id { get; set; }
+        public CrudStatus ModificationStatus { get; set; }
+    }
+}
---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Clients/EditClientRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Ssi.TrackTruck.Bussiness.DAL.Entities;
+
+namespace Ssi.TrackTruck.Bussiness.Clients
+{
+    public class EditClientRequest
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Please specify client's name")]
+        public string Name { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Truck per day cannot be less than zero")]
+        public int TrucksPerDay { get; set; }
+
+        public IList<DbTextItem> Addresses { get; set; }
+
+    }
+}
