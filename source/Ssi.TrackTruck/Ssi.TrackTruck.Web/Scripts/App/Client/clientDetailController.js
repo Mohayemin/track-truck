a47746c1d22c7783189ae2bdaa1f977916895785
Mohayemin <Mohayemin@localhost>
2015-10-17 18:59:37 +0600

    Client detail page updated

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientDetailController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Client/clientDetailController.js
@@ -2,14 +2,28 @@
     '$scope',
     '$routeParams',
     'clientService',
+    'userService',
     '$window',
     '$location',
     'globalMessage',
-    function($scope, $routeParams, clientService, $window,
+    function ($scope, $routeParams, clientService,
+        userService,
+        $window,
         $location,
         globalMessage) {
         function init(client) {
             $scope.client = client;
+
+            userService.getIndexedUsers().then(function(userIndex) {
+                $scope.client.Branches.forEach(function(branch) {
+                    branch.CustodianUser = userIndex[branch.CustodianUserId];
+                });
+            });
+
+
+            $scope.PickupAddressDisplay = client.Addresses.map(function (address) {
+                return address.Text;
+            }).join(', ');
 
             $scope.deleteClient = function() {
                 if ($window.confirm('Are you sure you want to delete this client?')) {
