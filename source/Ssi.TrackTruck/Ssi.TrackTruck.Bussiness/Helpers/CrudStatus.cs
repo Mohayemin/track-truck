ccc539870a61c01e7cea07c969ed47596662c91f
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2015-10-17 10:31:04 +0600

    crudStatus classes and services added to keep track of modification status

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Helpers/CrudStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Ssi.TrackTruck.Bussiness.Helpers
+{
+    // Keep sync with crudStatus.js
+    public enum CrudStatus
+    {
+        Unchanged = 0,
+        Added = 1,
+        Edited = 2,
+        Deleted = 3
+    }
+}
