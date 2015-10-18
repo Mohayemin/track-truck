de98ec9b5640155e34ede43f9ed740181163250f
Mohayemin <Mohayemin@localhost>
2015-10-18 07:12:15 +0600

    refactored client side dates

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/wellKnownDateTime.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/wellKnownDateTime.js
@@ -2,13 +2,24 @@
     '$filter',
     function ($filter) {
         var dateFilter = $filter('date');
-        return {
+        var service = {
             formatDate: function (date, format) {
                 return dateFilter(date, format || 'yyyy-MM-dd');
             },
             formatDateInt: function(date) {
                 return dateFilter(date, 'yyyyMMdd');
+            },
+            today: function() {
+                var today = new Date();
+                today.setHours(0, 0, 0, 0);
+                return today;
+            },
+            yesterday: function() {
+                var yesterday = service.today();
+                yesterday.setDate(yesterday.getDate() - 1);
+                return yesterday;
             }
         };
+        return service;
     }
 ]);
