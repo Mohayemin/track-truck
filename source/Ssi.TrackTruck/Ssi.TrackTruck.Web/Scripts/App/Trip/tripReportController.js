97968e789d07754fd617162d9d0bf07d9c41ef26
mohayemin <mohayemin@localhost>
2016-02-03 23:38:08 +0600

    functionalities of report template implemented. Not tested yet.

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/tripReportController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/tripReportController.js
@@ -7,10 +7,26 @@
         , wellKnownDateTime
         , tripService) {
 
-        $scope.filter = {
-            fromDate: wellKnownDateTime.yesterday(),
-            toDate: wellKnownDateTime.tomorrow()
+        function setDateRange(from, to) {
+            $scope.filter.fromDate = from;
+            $scope.filter.toDate = to;
+        }
+
+        $scope.filter = {};
+        setDateRange(wellKnownDateTime.yesterday(), wellKnownDateTime.tomorrow());
+        
+        $scope.setToday = function () {
+            setDateRange(wellKnownDateTime.today(), wellKnownDateTime.today());
         };
+
+        $scope.setThisWeek = function () {
+            setDateRange(wellKnownDateTime.weekStart(), wellKnownDateTime.weekEnd());
+        };
+
+        $scope.setThisMonth = function() {
+            setDateRange(wellKnownDateTime.monthStart(), wellKnownDateTime.monthEnd());
+        };
+
 
         $scope.loadReport = function () {
             tripService.getReport($scope.filter).then(function (trips) {
