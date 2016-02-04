2caef684e6563280838e7a42b772e807572bc519
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2016-02-04 23:06:51 +0600

    DbInitializer added

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/DbInitializer.cs
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/DAL/DbInitializer.cs
@@ -1,22 +1,49 @@
 ﻿using System;
 using System.Linq.Expressions;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
+using Ssi.TrackTruck.Bussiness.Auth;
 using Ssi.TrackTruck.Bussiness.DAL.Entities;
 using Ssi.TrackTruck.Bussiness.DAL.Trips;
 using Ssi.TrackTruck.Bussiness.DAL.Users;
 
 namespace Ssi.TrackTruck.Bussiness.DAL
 {
-    public class DbIndexBuilder
+    public class DbInitializer
     {
         private readonly MongoDatabase _db;
         private readonly CollectionMapper _mapper;
 
-        public DbIndexBuilder(MongoDatabase db, CollectionMapper mapper)
+        public DbInitializer(MongoDatabase db, CollectionMapper mapper)
         {
             _db = db;
             _mapper = mapper;
+        }
+
+        public void Init()
+        {
+            BuildIndexes();
+            InsertDefaultData();
+        }
+
+        public void InsertDefaultData()
+        {
+            var admin = new DbUser
+            {
+                Id = ObjectId.Empty.ToString(),
+                Username = "JR",
+                UsernameLowerCase = "jr",
+                PasswordHash = "g+S4Aydl1ZTXWYxO8IdfJWVUJVCpeTc7D09FOEFfPT/rvjDhVFVe9pqfIFS8HfU36AMAAA==",
+                Role = Role.Admin,
+                FirstName = "JRF",
+                LastName = "Ibanez",
+                IsDeleted = false,
+                CreationTime = DateTime.MinValue,
+                CreatorId = null
+            };
+
+            _db.GetCollection(_mapper.Map(typeof (DbUser))).Save(admin);
         }
 
         public void BuildIndexes()
