9e7c23e7c9b6c9439520749692f71161449e313d
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2016-02-20 15:02:17 +0600

    refactored

---
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Bussiness/Helpers/Util.cs
@@ -0,0 +1,10 @@
+﻿namespace Ssi.TrackTruck.Bussiness.Helpers
+{
+    public static class Util
+    {
+        public static long PessoToCentavos(double? pesso)
+        {
+            return (long) (pesso ?? 0)*100;
+        }
+    }
+}
