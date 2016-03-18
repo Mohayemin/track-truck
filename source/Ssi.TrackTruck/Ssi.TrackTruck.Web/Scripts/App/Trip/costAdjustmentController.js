77bbba1239206c32ea2d286e2aa2d99c42169a9b
mohayemin <mohayemin@localhost>
2016-03-18 11:46:40 +0600

    adjustment page updated

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/costAdjustmentController.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/App/Trip/costAdjustmentController.js
@@ -1,39 +1,56 @@
 ﻿tripModule.controller('costAdjustmentController', [
     '$scope',
     'tripService',
+    'costType',
     'globalMessage',
     '$routeParams',
     function orderTripController(
         $scope,
         tripService,
+        costType,
         globalMessage,
         $routeParams) {
+
+        function Adjustment(cost) {
+            this.Id = cost.Id;
+            this.ExpectedCostInPeso = cost.ExpectedCostInPeso || 0;
+            this.ActualCostInPeso = cost.ActualCostInPeso || this.ExpectedCostInPeso;
+            this.CostType = cost.CostType || costType.Discrepancy;
+        }
+
+        Adjustment.prototype.getAdjustment = function() {
+            return this.ExpectedCostInPeso - this.ActualCostInPeso;
+        };
 
         
         tripService.get($routeParams['id']).then(function (trip) {
             $scope.trip = trip;
-            $scope.adjustments = trip.Costs.map(function(cost) {
-                return {
-                    Id: cost.Id,
-                    ExpectedCostInPeso: cost.ExpectedCostInPeso,
-                    ActualCostInPesso: cost.ActualCostInPesso,
-                    CostType: cost.CostType
-                }
+            $scope.adjustments = trip.Costs.map(function (cost) {
+                return new Adjustment(cost);
             });
+            $scope.total = new Adjustment({
+                ExpectedCostInPeso: _.sumBy($scope.adjustments, function (a) { return a.ExpectedCostInPeso; })
+            });
+
+            $scope.recalculateTotal();
         });
 
-        $scope.save = function() {
+        $scope.save = function () {
             tripService.saveAdjustment($scope.trip, $scope.adjustments);
         };
 
         $scope.addNewAdjustment = function () {
-            $scope.adjustments.push({});
+            $scope.adjustments.push(new Adjustment({}));
         };
 
         $scope.deleteAdjustment = function (adjustment) {
-            _.remove($scope.adjustments, function(a) {
+            _.remove($scope.adjustments, function (a) {
                 return adjustment === a;
             });
+        };
+
+        $scope.recalculateTotal = function () {
+            $scope.total.ActualCostInPeso = _.sumBy($scope.adjustments, function (a) { return a.ActualCostInPeso; });
         };
     }
 ]);
