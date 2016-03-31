23b87c7413e244107d453939dcf85a85a127e61d
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2016-03-31 23:54:44 +0600

    dateTimePicker added

--- source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/datetimeDirective.js
+++ source/Ssi.TrackTruck/Ssi.TrackTruck.Web/Scripts/Util/datetimeDirective.js
@@ -6,7 +6,16 @@
                 ngModel: '=',
                 hideTime: '=',
                 options: '='
-            }
+            },
+            controller: [
+                '$scope', '$rootScope',
+                function ($scope, $rootScope) {
+                    $scope.dateFormat = $rootScope.dateFormat;
+                    $scope.dateTimeFormat = $rootScope.dateTimeFormat;
+
+                    console.log($scope.hideTime ? $scope.dateFormat : $scope.dateTimeFormat);
+                }
+            ]
         };
     }
 ]);
